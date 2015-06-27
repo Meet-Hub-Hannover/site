6252204e8b1e50971fca0bdf8e2364642e7cc0f4
Tobias Baum <tbaum@tntinteractive.de>
2015-06-27 15:47:06 +0200

    api key auf kommandozeile umgestellt

--- mailer/run_mailer.sh
+++ mailer/run_mailer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
-java -cp './*' de.meethub.mailer.Mailer $MAILCHIMP_APIKEY a76317b2eb http://localhost:8080/Meet-Hub-Hannover/mergedCalendar.ics
+#mailchimp api key has to be supplied as parameter
+java -cp './*' de.meethub.mailer.Mailer $1 a76317b2eb http://localhost:8080/Meet-Hub-Hannover/mergedCalendar.ics
