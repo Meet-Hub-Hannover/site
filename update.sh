2949538797f4cd52794a99cfedda2ec1241ce915
Tobias Baum <tbaum@tntinteractive.de>
2015-06-27 15:19:13 +0200

    Restart nur wenn nötig

--- update.sh
+++ update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-git pull
-./jetty/bin/jetty.sh restart
-cp ./default.vcl /etc/varnish/
-cp ./varnish /etc/sysconfig/
-service varnish restart
+cd /srv/stor/git/Meet-Hub-Hannover/site
+git pull >pull.txt
+if grep -q 'Already up-to-date' <pull.txt; then
+    exit
+fi
+./jetty/bin/jetty.sh restart 2>&1 1>updateLog.txt
+cp ./default.vcl /etc/varnish/ 2>&1 1>>updateLog.txt
+cp ./varnish /etc/sysconfig/ 2>&1 1>>updateLog.txt
+service varnish restart 2>&1 1>>updateLog.txt
+echo 'update done' >>updateLog.txt
