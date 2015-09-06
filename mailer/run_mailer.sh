ccc8e58d5c87d7ee5d91ba2048be6764e488506c
tbaum <tbaum>
2015-09-06 11:14:32 +0000

    Anpassungen am mailer-Skript

--- mailer/run_mailer.sh
+++ mailer/run_mailer.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #mailchimp api key has to be supplied as parameter
-java -cp './*' de.meethub.mailer.Mailer $1 a76317b2eb http://localhost:8080/Meet-Hub-Hannover/mergedCalendar.ics
+cd /srv/stor/git/Meet-Hub-Hannover/site/mailer
+java -cp './*' de.meethub.mailer.Mailer $1 a76317b2eb http://localhost:8080/Meet-Hub-Hannover/mergedCalendar.ics 2>&1 >mailer.log
