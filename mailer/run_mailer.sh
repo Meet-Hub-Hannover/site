a554db41e39f1a773c5c7801ceca9fa5f0b26491
Tobias Baum <tbaum@tntinteractive.de>
2015-09-12 11:06:41 +0200

    Skript an neue Mail-Version angepasst

--- mailer/run_mailer.sh
+++ mailer/run_mailer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #mailchimp api key has to be supplied as parameter
 cd /srv/stor/git/Meet-Hub-Hannover/site/mailer
-java -cp './*' de.meethub.mailer.Mailer $1 a76317b2eb http://localhost:8080/Meet-Hub-Hannover/mergedCalendar.ics 2>&1 >mailer.log
+java -cp './*' de.meethub.mailer.Mailer $1 a76317b2eb http://localhost:8080/Meet-Hub-Hannover/mergedCalendar.ics ../usergroups 2>&1 >mailer.log
